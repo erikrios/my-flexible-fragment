e6bbd6e79a88dadd9354f638afbb35e605a88d8d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-20 19:28:48 +0700

    Integrated the data binding

---
+++ app/src/main/java/io/erikrios/github/myflexiblefragment/OptionDialogFragment.kt
@@ -0,0 +1,27 @@
+package io.erikrios.github.myflexiblefragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.erikrios.github.myflexiblefragment.databinding.FragmentOptionDialogBinding
+
+class OptionDialogFragment : Fragment() {
+
+    private var _binding: FragmentOptionDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOptionDialogBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
