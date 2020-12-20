263373dcc62cf4acd28fad59bf80207170d84997
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-20 11:40:02 +0700

    Add fragment in activity

--- app/src/main/java/io/erikrios/github/myflexiblefragment/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myflexiblefragment/MainActivity.kt
@@ -1,11 +1,30 @@
 package io.erikrios.github.myflexiblefragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.myflexiblefragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name : ${HomeFragment::class.java.simpleName}")
+            mFragmentManager
+                .beginTransaction()
+                .add(binding.frameContainer.id, mHomeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
