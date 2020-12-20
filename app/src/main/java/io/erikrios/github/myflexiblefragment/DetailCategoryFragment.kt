16233c9674623cd326ea9f5a709a879c9d9657f9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-20 18:59:01 +0700

    Implements the view binding

---
+++ app/src/main/java/io/erikrios/github/myflexiblefragment/DetailCategoryFragment.kt
@@ -0,0 +1,32 @@
+package io.erikrios.github.myflexiblefragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.erikrios.github.myflexiblefragment.databinding.FragmentDetailCategoryBinding
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailCategoryBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
